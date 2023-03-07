870fe43875d89db4148dd1c336b6015f79867c6b
Janis M. Baumann <janis.baumann@elrem.ch>
2023-03-07 18:45:26 +0100

    styling von detail view verbessert, charts styling begonnen

--- components/Chart.vue
+++ components/Chart.vue
@@ -36,6 +36,37 @@ const data = {
   datasets: lines
 }
 //https://www.chartjs.org/docs/latest/samples/tooltip/content.html
+const totalDuration = 2000;
+const delayBetweenPoints = totalDuration / legend.length;
+const previousY = (ctx) => ctx.index === 0 ? ctx.chart.scales.y.getPixelForValue(100) : ctx.chart.getDatasetMeta(ctx.datasetIndex).data[ctx.index - 1].getProps(['y'], true).y;
+const animation = {
+  x: {
+    type: 'number',
+    easing: 'linear',
+    duration: delayBetweenPoints,
+    from: NaN, // the point is initially skipped
+    delay(ctx) {
+      if (ctx.type !== 'data' || ctx.xStarted) {
+        return 0;
+      }
+      ctx.xStarted = true;
+      return ctx.index * delayBetweenPoints;
+    }
+  },
+  y: {
+    type: 'number',
+    easing: 'linear',
+    duration: delayBetweenPoints,
+    from: previousY,
+    delay(ctx) {
+      if (ctx.type !== 'data' || ctx.yStarted) {
+        return 0;
+      }
+      ctx.yStarted = true;
+      return ctx.index * delayBetweenPoints;
+    }
+  }
+};
 const options = {
     responsive: true,
     plugins: {
@@ -44,6 +75,7 @@ const options = {
             text: 'Chart.js Line Chart'
         },
     },
+    animation,
     maintainAspectRatio: true,
     scales: {
         x: {
@@ -56,7 +88,7 @@ const options = {
             }
         },
         y: {
-
+            
         }
 
     }
--- components/StationDetailView.vue
+++ components/StationDetailView.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="py-0 px-0 bg-white rounded-md shadow-md w-full">
+    <div class="min-h-screen py-0 px-0 bg-white rounded-md shadow-md w-full">
         <div class="px-4 py-8 justify-center flex w-full">
             <h1 class="text-4xl text-400">{{ windData.name }}</h1>
         </div>
-        <div class="min-h-screen container my-2 mx-auto px-0 md:px-4 grid md:grid-cols-2 sm:grid-cols-1">
-            <div class="lg:mx-20 md:mx-16 mx-10 sm:mb-20 justify-center items-center md:p-20">
+        <div class="container my-2 mx-auto px-0 md:px-4 grid md:grid-cols-2 sm:grid-cols-1">
+            <div class="mx-16 mb-0 justify-center items-center p-20">
                 <div class="square bg-rose bg-contain bg-no-repeat justify-center items-center">
                     <img class="square flex justify-center items-center" src="~/assets/images/compass_inside.png" :style="{transform: `rotate(${windData.vent_direction + 180}deg)`}">
                 </div>
                 <div class="flex justify-center items-center text-4xl mt-10 text-bold">{{ windData.vent_direction_cardinal }} {{ windData.vent_direction }}&deg</div>
             </div> 
-            <div class="w-full h-full flex">
-                <div class="w-full h-full md:px-10 grid grid-rows-2 items-center">
+            <div class="w-full flex">
+                <div class="w-full md:px-10 px-4 grid grid-rows-2 items-center">
                     <div class="grid grid-cols-2 md:gap-10 gap-4 mb-14">
                         <div class="shadow-inner rounded-md bg-rose_very_light">
                             <ColorGradient :windspeed="windData.vent_vitesse" direction="to-t" class="overflow-clip" addClass="rounded-md">
@@ -43,8 +43,12 @@
                 </div>
             </div>
         </div>
-        <div class="w-full flex px-4 pb-4 justify-center">
-            <Chart class="w-full" :lines="datasetsChart1" :legend="pointsTime"/>
+        <h2 class="text-4xl flex justify-center mb-8">Hisytory</h2>
+        <div class="px-4 pb-4 flex justify-center w-full">
+            <Chart class="flex justify-center sm:w-full md:max-w-6xl" :lines="datasetsChart1" :legend="pointsTime"/>
+        </div>
+        <div class="px-4 pb-4 flex justify-center w-full my-8">
+            <Chart class="flex justify-center sm:w-full md:max-w-6xl" :lines="datasetsChart2" :legend="pointsTime"/>
         </div>
     </div>
 </template>
@@ -63,7 +67,7 @@ const pointsDirection = []
 console.log(history)
 
 history.map((element, key) => {
-    pointsTime.push(new Date(element[0]).toLocaleTimeString('ch-de', { hour: '2-digit', minute: '2-digit' }))
+    pointsTime.push(new Date(element[0]).toLocaleString('ch-de', { weekday: 'short' , hour: '2-digit', minute: '2-digit' }))
     pointsAvg.push(element[1])
     pointsGust.push(element[2])
     pointsDirection.push(element[3])
@@ -83,6 +87,17 @@ const datasetsChart1 = [
       backgroundColor: '#C80000',
       borderColor: '#C80000',
       data: pointsGust,
+      tension: 0.4,
+      pointStyle: false,
+    },
+  ]
+
+const datasetsChart2 = [
+    {
+      label: 'Wind Direction',
+      backgroundColor: '#1C2834',
+      borderColor: '#1C2834',
+      data: pointsDirection,
       tension: 0.4,
       pointStyle: false,
     },
