3cbc9191d5c59ffa39113a6f3ab3f6052f2c68a9
Janis M. Baumann <janis.baumann@elrem.ch>
2023-05-24 18:44:44 +0200

    server für app geändert

--- server/api/station_details/[id].js
+++ server/api/station_details/[id].js
@@ -12,11 +12,6 @@ export default defineEventHandler(async (event) => {
             wind_direction_text: data.vent_direction_cardinal,
             wind_speed: data.vent_vitesse,
             wind_gust: data.vent_rafale,
-            last_update: data.last_update,
-            last_update_min: data.last_update_min,
-            source_link: data.source_lien,
-            latitute: data.latitute,
-            longitude: data.longitude,
             temperature: data.temp 
         }
 })
