30ca59ed80831d0b762af7de744b0d19b02a9cc9
Janis M. Baumann <janis.baumann@elrem.ch>
2023-05-24 13:20:19 +0200

    added keywords

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,9 +8,13 @@ export default defineNuxtConfig({
     ],
     app: {
         head: {
-            title: "Le Wind",
+            title: "Le Wind | Live Wind Data",
             meta: [
-                { name: 'description', content: 'Find hot wind-speeds in your local Area' }
+                { name: 'description', content: 'Observe at your favorite Kitesurf and Windsurf Spots and never miss a Session with Le Wind!' },
+                { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
+                { name: 'HandheldFriendly', content: 'true' },
+                { name: 'author', content: 'Janis M. Baumann' },
+                { name: 'keywords', content: 'wind gust gusts le kite kiting kitesurfing live data windsurfing windsurf kitesurf weather windmessung wind messung windmessungen windmessung gerät wind gerät gerät zur windmessung livedaten measure measurement kitesurfen surfen windsurfen windmessung wind messung windmessungen windmessung gerät wind gerät gerät zur windmessung kitesurf livedaten wetter windsurfen wassersport wetter morgen wetter heute wetterradar wettervorhersage das wetter wetterbericht biowetter das wetter heute wetter aktuell wettertrend das wetter morgen morgen wetter heute wetter reisewetter wetterkarte wettero wetterbericht morgen wetter meteo wetter für morgen wetter für heute wettervorhersage morgen aktuelles wetter wetter vorhersage wetter von heute meteo wetter wetter in wetterfühligkeit wind surfen wasser sport windsurfen windstärke surfen wind windstärke windsurfen windsurfen wind surfen windsurfen surfen windstärke wind windsurfen schweiz switzerland bern berne' },
             ],
             link: [
                 { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
